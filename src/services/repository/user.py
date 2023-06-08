b1e8207220d5bf8dcc359c7c9a402b6fbf03d7d9
JKearnsl <boyarshinovn@gmail.com>
2023-06-08 16:57:03 +0300

    Изменение ролей

--- src/services/repository/user.py
+++ src/services/repository/user.py
@@ -13,8 +13,3 @@ class UserRepo(BaseRepository[tables.User]):
         return (await self._conn.execute(
             select(self.table).where(func.lower(self.table.username) == username.lower())
         )).scalar_one_or_none()
-
-    async def get_by_email_insensitive(self, email: str) -> Optional[tables.User]:
-        return (await self._conn.execute(
-            select(self.table).where(func.lower(self.table.email) == email.lower())
-        )).scalar_one_or_none()
