c49b6ca5c3bfde12a0ad832fa1e4977289014cfd
JKearnsl <boyarshinovn@gmail.com>
2023-06-08 16:56:50 +0300

    Обновлены требования

--- src/app.py
+++ src/app.py
@@ -3,17 +3,16 @@ import logging
 import redis.asyncio as redis
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
-from fastapi.openapi.utils import get_openapi
 
 from src.models import tables
-from src.db import create_psql_async_session, create_sqlite_async_session
+from src.db import create_sqlite_async_session
 from src.middleware import JWTMiddlewareHTTP
 from src.config import load_ini_config
 from src.exceptions import APIError, handle_api_error, handle_404_error, handle_pydantic_error
 
 from src.router import reg_root_api_router
-from src.services.storage.s3 import S3Storage
-from src.utils import RedisClient, AiohttpClient
+from src.utils import RedisClient
+from src.utils.openapi import custom_openapi
 
 config = load_ini_config('./config.ini')
 log = logging.getLogger(__name__)
@@ -33,22 +32,6 @@ app = FastAPI(
         "email": config.BASE.CONTACT.EMAIL,
     }
 )
-
-
-async def init_postgresql_db():
-    engine, session = create_psql_async_session(
-        username=config.DB.POSTGRESQL.USERNAME,
-        password=config.DB.POSTGRESQL.PASSWORD,
-        host=config.DB.POSTGRESQL.HOST,
-        port=config.DB.POSTGRESQL.PORT,
-        database=config.DB.POSTGRESQL.DATABASE,
-        echo=config.DEBUG,
-    )
-    app.state.db_session = session
-
-    async with engine.begin() as conn:
-        # await conn.run_sync(tables.Base.metadata.drop_all)
-        await conn.run_sync(tables.Base.metadata.create_all)
 
 
 async def init_sqlite_db():
@@ -71,74 +54,27 @@ async def redis_pool(db: int = 0):
     )
 
 
-def init_file_storage():
-    # s3 storage
-    app.state.file_storage = S3Storage(
-        bucket=config.DB.S3.BUCKET,
-        service_name=config.DB.S3.SERVICE_NAME,
-        endpoint_url=config.DB.S3.ENDPOINT_URL,
-        region_name=config.DB.S3.REGION_NAME,
-        aws_access_key_id=config.DB.S3.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=config.DB.S3.AWS_SECRET_ACCESS_KEY,
-    )
-
-
 @app.on_event("startup")
 async def on_startup():
-    log.debug("Executing FastAPI startup event handler.")
-    if config.DB.POSTGRESQL:
-        await init_postgresql_db()
-    else:
-        await init_sqlite_db()
-    if config.DB.REDIS:
-        app.state.redis = RedisClient(await redis_pool())
-    if config.DB.S3:
-        init_file_storage()
-    app.state.http_client = AiohttpClient()
-    print("FastAPI startup event handler executed.")
+    log.debug("Выполнение FastAPI startup event handler.")
+    await init_sqlite_db()
+    app.state.redis = RedisClient(await redis_pool())
 
 
 @app.on_event("shutdown")
 async def on_shutdown():
-    log.debug("Executing FastAPI shutdown event handler.")
-    # Gracefully close utilities.
-    if config.DB.REDIS:
-        await app.state.redis.close()
-    await app.state.http_client.close_session()
+    log.debug("Выполнение FastAPI shutdown event handler.")
+    await app.state.redis.close()
 
 
-# custom OpenApi
-def custom_openapi():
-    if not app.openapi_schema:
-        app.openapi_schema = get_openapi(
-            title=app.title,
-            version=app.version,
-            openapi_version=app.openapi_version,
-            description=app.description,
-            terms_of_service=app.terms_of_service,
-            contact=app.contact,
-            license_info=app.license_info,
-            routes=app.routes,
-            tags=app.openapi_tags,
-            servers=app.servers,
-        )
-        for _, method_item in app.openapi_schema.get('paths').items():
-            for _, param in method_item.items():
-                responses = param.get('responses')
-                # remove 422 response, also can remove other status code
-                if '422' in responses:
-                    del responses['422']
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
+app.openapi = lambda: custom_openapi(app)
 app.state.config = config
 
-log.debug("Adding routers.")
+log.debug("Добавление маршрутов")
 app.include_router(reg_root_api_router(config.DEBUG))
-log.debug("Registering exception handlers.")
+log.debug("Регистрация обработчиков исключений.")
 app.add_exception_handler(APIError, handle_api_error)
 app.add_exception_handler(404, handle_404_error)
 app.add_exception_handler(RequestValidationError, handle_pydantic_error)
-log.debug("Registering middleware.")
+log.debug("Регистрация middleware.")
 app.add_middleware(JWTMiddlewareHTTP)
