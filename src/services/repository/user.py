061a39471c8371e8d57139951659baf43a4bb706
JKearnsl <boyarshinovn@gmail.com>
2023-06-15 12:50:42 +0300

    Мелкие правки

--- src/services/repository/user.py
+++ src/services/repository/user.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from sqlalchemy import insert, update, delete, func, select
 
 from src.models import tables
@@ -9,7 +7,7 @@ from src.services.repository.base import BaseRepository
 class UserRepo(BaseRepository[tables.User]):
     table = tables.User
 
-    async def get_by_username_insensitive(self, username: str) -> Optional[tables.User]:
+    async def get_by_username_insensitive(self, username: str) -> tables.User | None:
         return (await self._session.execute(
             select(self.table).where(func.lower(self.table.username) == username.lower())
         )).scalar_one_or_none()
